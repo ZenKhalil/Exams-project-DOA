3867c5622cdf567a9d43f006aa64c8af59b510b9
Zen Khalil <feattzen@gmail.com>
2025-01-05 12:30:05 +0100

    Added better comments

--- datastructures/CircularBuffer.js
+++ datastructures/CircularBuffer.js
@@ -1,33 +1,36 @@
 class CircularBuffer {
   constructor(size) {
+    // Initialize buffer with fixed size
     this.size = size;
-    this.buffer = new Array(size);
-    this.current = 0;
-    this.count = 0;
+    this.buffer = new Array(size); 
+    this.current = 0; 
+    this.count = 0; 
   }
 
   add(step) {
-    this.buffer[this.current] = step;
-    this.current = (this.current + 1) % this.size;
-    this.count = Math.min(this.count + 1, this.size);
+    // Add a step to the buffer
+    this.buffer[this.current] = step; 
+    this.current = (this.current + 1) % this.size; 
+    this.count = Math.min(this.count + 1, this.size); 
   }
 
   getLast(n = 5) {
+    // Retrieve the last `n` steps
     const steps = [];
-    let index = this.current - 1;
-
+    let index = this.current - 1; 
     for (let i = 0; i < Math.min(n, this.count); i++) {
-      if (index < 0) index = this.size - 1;
-      steps.unshift(this.buffer[index]);
+      if (index < 0) index = this.size - 1; 
+      steps.unshift(this.buffer[index]); 
       index--;
     }
 
-    return steps;
+    return steps; // Return the last `n` steps
   }
 
   clear() {
-    this.buffer = new Array(this.size);
+    // Clear the buffer
+    this.buffer = new Array(this.size); 
     this.current = 0;
-    this.count = 0;
+    this.count = 0; 
   }
 }
